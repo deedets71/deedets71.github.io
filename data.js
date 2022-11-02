9bc8f04b700a4dffcd2461eb93590660cb95afe0
deedets71 <deedets71@gmail.com>
2022-11-03 04:46:43 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,188 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360-room-hotel-1",
+      "name": "360 ROOM HOTEL 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5931604402892887,
+        "pitch": -0.00037042951185028983,
+        "fov": 0.7076169365003416
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07553929475131049,
+          "pitch": 0.7537255670370406,
+          "rotation": 0,
+          "target": "1-360-room-hotel-2"
+        },
+        {
+          "yaw": 0.45271702265920055,
+          "pitch": 0.27614200445104053,
+          "rotation": 0,
+          "target": "2-360-room-hotel-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.16368269908073074,
+          "pitch": 0.7592633400712856,
+          "title": "View 2",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.5175797972260519,
+          "pitch": 0.27560684165958804,
+          "title": "View 3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-360-room-hotel-2",
+      "name": "360 ROOM HOTEL 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.5621012032570025,
+        "pitch": 0.029610213116672668,
+        "fov": 0.8631310226132937
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0847450130305312,
+          "pitch": 0.6474499341074562,
+          "rotation": 0,
+          "target": "0-360-room-hotel-1"
+        },
+        {
+          "yaw": 2.926030757847723,
+          "pitch": 0.4000100890393625,
+          "rotation": 0,
+          "target": "2-360-room-hotel-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1043292953336667,
+          "pitch": 0.5875338026251882,
+          "title": "View 1",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-360-room-hotel-3",
+      "name": "360 ROOM HOTEL 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5398093681881617,
+        "pitch": -0.00539965715633528,
+        "fov": 0.8587833770818601
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.945938237590335,
+          "pitch": 0.45332455932033966,
+          "rotation": 0,
+          "target": "1-360-room-hotel-2"
+        },
+        {
+          "yaw": -2.8266503273862895,
+          "pitch": 0.26875301396034246,
+          "rotation": 0,
+          "target": "0-360-room-hotel-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8794063956666207,
+          "pitch": 0.44532269010026937,
+          "title": "View 1",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.7772146197337566,
+          "pitch": 0.2651081694326063,
+          "title": "View 1",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Room Hotel 1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
